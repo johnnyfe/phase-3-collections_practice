1b9e7eea0a3ff0b7bb0ea4abfcaaa1f0efa8a829
johnny <johnny1194@hotmail.com>
2021-07-04 13:10:45 -0400

    Completed assignment

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc (array)
+    array.sort
+end
 
+def sort_array_desc (array)
+    array.sort do |a, b|
+        b <=> a
+      end
+end
+
+def sort_array_char_count (array)
+    array.sort do |a, b|
+        a.length <=> b.length
+      end
+end
+
+def swap_elements (array)
+     array[1], array[2] = array[2], array[1]
+     array
+end
+
+def reverse_array (array)
+    array.reverse
+end
+
+def kesha_maker (array)
+    array.each do |letter|
+    letter[2]="$"
+    end
+end
+
+def find_a (array)
+    array.find_all {|letter| letter[0]=="a"}
+end
+
+def sum_array (array)
+    array.inject(:+)
+end
+
+def add_s (array)
+    array.collect do |element|
+        if array[1] == element
+             element
+        else 
+            element+"s"
+        end
+    end
+end
